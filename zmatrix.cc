276e81a35344304237db7ad955f250cf778e169d
peterjohncherry <peter.cherry@northwestern.edu>
2019-05-02 16:34:18 +0200

    fixed stupid allocation bug in zmatrix diagonalization

--- zmatrix.cc
+++ zmatrix.cc
@@ -184,8 +184,8 @@ void ZMatrix::generate_stdcomplex_data(){
 
 void diagonalize_complex_routine(std::unique_ptr<ZMatrix>& mat ) {
   
-   char JOBVL = 'V';
-   char JOBVR = 'V';
+   char JOBVL = 'N';
+   char JOBVR = 'N';
    int N = mat->ncols();
    double* A = mat->complex_data_ptr();
    int LDA = mat->nrows();
@@ -243,66 +243,60 @@ void diagonalize_complex_routine(std::unique_ptr<ZMatrix>& mat ) {
 }
 
 void ZMatrix::diagonalize_stdcomplex_routine() {
-  
+   
    char JOBVL = 'V';
    char JOBVR = 'V';
    int N = ncols_;
    lapack_complex_double* A = (lapack_complex_double*)(stdcomplex_data_.get());
    int LDA = nrows_;
    int LDB = nrows_;
-
-   cout << "nrows_ = " << nrows_ << endl;
-   cout << "ncols_ = " << ncols_ << endl;
-
-   unique_ptr<double[]> B_array = make_unique<double[]>(LDB*N*4);
-   lapack_complex_double* B = (lapack_complex_double*) B_array.get();
-
-   unique_ptr<double[]> ALPHA_array = make_unique<double[]>(N*2);
-   lapack_complex_double* ALPHA = (lapack_complex_double*)ALPHA_array.get();
-
-   unique_ptr<double[]> BETA_array = make_unique<double[]>(N*2);
-   lapack_complex_double* BETA = (lapack_complex_double*)BETA_array.get();
-
    int LDVL = N;
    int LDVR = N;
-   unique_ptr<double[]> VL_array = make_unique<double[]>(N*2);
-   lapack_complex_double* VL = (lapack_complex_double*)VL_array.get();
-
-   unique_ptr<double[]> VR_array = make_unique<double[]>(N*2);
-   lapack_complex_double* VR = (lapack_complex_double*)VR_array.get();
-
    int LWORK = 3*N; // GET THIS PROPERLY
-
+   
+   unique_ptr<double[]> B_array = make_unique<double[]>(LDB*N*4);
+   lapack_complex_double* B = (lapack_complex_double*) B_array.get();
+   
+   unique_ptr<double[]> ALPHA_array = make_unique<double[]>(N*2);
+   lapack_complex_double* ALPHA = (lapack_complex_double*)ALPHA_array.get();
+   
+   unique_ptr<double[]> BETA_array = make_unique<double[]>(N*2);
+   lapack_complex_double* BETA = (lapack_complex_double*)BETA_array.get();
+  
+   lapack_complex_double* VL;
+   unique_ptr<double[]> VL_array;
+   switch (JOBVL){ 
+     case (int)'V' : 
+       VL_array = make_unique<double[]>(N*2*LDVR);
+       VL = (lapack_complex_double*)VL_array.get();
+       break;
+     case (int)'N' :
+       break;
+     default :
+       throw std::logic_error("ZMatrix::diagonalize JOBVL must be either 'V' or 'N' !!");
+   } 
+ 
+   lapack_complex_double* VR;
+   unique_ptr<double[]> VR_array;
+   switch (JOBVR){ 
+     case (int)'V' : 
+       VR_array = make_unique<double[]>(N*2*LDVR);
+       VR = (lapack_complex_double*)VR_array.get();
+       break;
+     case (int)'N' :
+       break;
+     default :
+       throw std::logic_error("ZMatrix::diagonalize JOBVR must be either 'V' or 'N' !!");
+   } 
+   
    unique_ptr<double[]> WORK_array = make_unique<double[]>(LWORK*2);
    lapack_complex_double* WORK = (lapack_complex_double*)WORK_array.get();
-
+   
    unique_ptr<double[]> RWORK_array = make_unique<double[]>(8*N*2);
    double* RWORK = RWORK_array.get();
    int INFO = 0;
 
-   cout << "matrix" << endl;
-   print();
-//   cout << "*A = "<< *A << endl;
-   cout << endl << endl << " data" << endl;
-   for ( int ii = 0 ; ii != 9; ++ii, ++A) { 
-     cout << *(A) << " " ; cout.flush();
-   }
-   A =  (lapack_complex_double*)(stdcomplex_data_.get());
-
-   cout << endl;
-
-   cout << "X10" << endl;
    zggev_( &JOBVL, &JOBVR, &N, A, &LDA, B, &LDB, ALPHA, BETA, VL,
            &LDVL, VR, &LDVR, WORK, &LWORK, RWORK, &INFO );
-
-   print_array(ALPHA_array.get(), N*2, "Alpha_array"); cout << endl;
-   
-   print_array(BETA_array.get(), N*2, "Beta_array"); cout << endl;
-   print_array(B_array.get(), LDB*N*2, "B_array"); cout << endl;
-   print_array(RWORK_array.get(), LDB*N*2, "RWORK"); cout << endl;
-   print_array(WORK_array.get(), LWORK*2, "WORK"); cout << endl;
-   print_array(VL_array.get(), N*2, "VL_array"); cout << endl;
-   print_array(VR_array.get(), N*2, "VR_array"); cout << endl;
-
-   cout << "X11" << endl;
+  
 }
