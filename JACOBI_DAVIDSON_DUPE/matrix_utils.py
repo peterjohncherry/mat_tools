90d2612f3910c6ffa81b91453c2ee470a95336d9
peterjohncherry <peter.cherry@northwestern.edu>
2019-07-12 18:22:15 +0200

    Problem with orthogonalization, and probably guess
    orthogonalization is baffling, as it should be fine...

--- JACOBI_DAVIDSON_DUPE/matrix_utils.py
+++ JACOBI_DAVIDSON_DUPE/matrix_utils.py
@@ -10,7 +10,14 @@ def symmetrize_mat(mat):
 # generates a real random matrix with nrows and ncols
 def generate_random_symmetric_matrix(nrows, ncols):
     # type: (int, int) -> matrix
-    return symmetrize_mat(np.random.rand(nrows, ncols))
+    mat =np.ndarray((nrows, ncols))
+    for ii in range(nrows):
+        for jj in range(ncols):
+            if ii == jj:
+                mat[ii,jj] =ii*ii+1
+            else :
+                mat[ii,jj] = np.random.rand()
+    return symmetrize_mat(mat)
 
 #Scales down off diagonal elements to make matrix amenable to solution by Davidson
 def make_diagonally_dominant(mat_in, sparsity) :
@@ -26,6 +33,65 @@ def make_diagonally_dominant(mat_in, sparsity) :
 def orthonormalize(vec, mat):
     for ii in range(np.size(mat,1)):
         vec = vec - np.dot(vec, mat[:,ii])*mat[:,ii]
-
     return vec/np.linalg.norm(vec)
 
+def test_orthogonality(A, name ="A"):
+    AtA = np.dot(A.T, A)
+    for ii in range(AtA.shape[0]):
+        for jj in range(AtA.shape[1]):
+            if (ii != jj and np.abs(AtA[ii,jj]) > 1e-3):
+                print(name+"^{T}"+name+"[",ii,",",jj,"] = ", AtA[ii,jj])
+                sys.exit("Orthogonalization failed!")
+
+def modifiedGramSchmidt(A):
+    ncols = A.shape[1]
+    Q = np.zeros(A.shape, dtype=A.dtype)
+    for j in range(ncols):
+        q = A[:,j]
+        for i in range(j):
+            rij = np.vdot(Q[:,i],q)
+            q = q - rij*Q[:,i]
+        rjj = np.linalg.norm(q, ord=2)
+        if np.isclose(rjj,0.0):
+            raise ValueError("invalid input matrix")
+        else:
+            Q[:,j] = q/rjj
+    return Q
+
+#Assumes A is already orthogonormalized
+def orthonormalize_v_against_A(v, A):
+    ncols = A.shape[1]
+    for ii in range(ncols):
+        v = v - np.vdot(A[:,ii],v)*A[:,ii]
+    modv = np.linalg.norm(v)
+    v = v/modv
+    return v
+
+def print_only_large_imag( vec,name = " " ):
+    if name != " ":
+        print(name, end=' = ')
+    for elem in vec :
+        if ( abs(elem.imag) < 0.000001):
+            print (elem.real, end=' ')
+        else:
+            print(elem, end=' ')
+    print ("\n")
+
+def sort_eigvecs_and_vals(eigvals, eigvecs):
+
+    idx = eigvals.argsort()
+    eigvals = eigvals[idx]
+    eigvecs = eigvecs[:, idx]
+
+#checks whether columns of matrix vspace are normalized
+def check_normalization(vspace, thresh = 1e-10, name = "???"):
+    bad_norms = []
+
+    for ii in range(iter):
+        vnorm =np.linalg.norm(self.vspace[:, ii])
+        if ( vnorm -1 > thresh ):
+            vnorms.append((str(ii)+ "norm = ", vnorm ))
+    if len(vnorm) > 0:
+        for a,b in vnorms :
+            print(a,b)
+        sys.exit("normalization of " + name + " failed... Aborting!")
--- JACOBI_DAVIDSON_DUPE/tests.py
+++ JACOBI_DAVIDSON_DUPE/tests.py
@@ -1,20 +1,26 @@
 
 import eps_solvers
 import matrix_utils as mu
+import numpy as np
 
 def test_jacobi_davidson():
     threshold = 0.0000001
     max_iter = 20
-    jd_test = eps_solvers.eps_solver("Jacobi Davidson", threshold, max_iter)
+    jd_test = eps_solvers.eps_solver("Jacobi Davidson", threshold, max_iter, preconditioning_type="Full")
 
-    ndim = 20
+    ndim = 50
     nev = 4
-    sparsity = 0.0001
+    sparsity = 0.01
     A = mu.make_diagonally_dominant( mu.generate_random_symmetric_matrix(ndim, ndim), sparsity)
-
     jd_test.set_variables(ndim, nev, A)
     jd_test.first_iteration_init()
     jd_test.main_loop()
+
+    npvals, npvecs = np.linalg.eig(A)
+    npvals.sort()
+    npvals = npvals[::-1]
+    print ("npvals = ", npvals)
+
 
 
 
