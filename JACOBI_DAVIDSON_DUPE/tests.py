e2d1c5cb6fe1d5a904bf636cc558ac38f092bdc8
peterjohncherry <peter.cherry@northwestern.edu>
2019-07-28 15:07:25 +0200

    Removed all files for solvers not comparable to RS

--- JACOBI_DAVIDSON_DUPE/tests.py
+++ JACOBI_DAVIDSON_DUPE/tests.py
@@ -1,10 +1,7 @@
 import matrix_utils as mu
 import numpy as np
-import davidson
-import jacobi_davidson_real as jdr
 import jacobi_davidson_4c as jdr4c
 import mat_reader as mr
-import tddft_info as Info
 
 def numpy_check( matrix_orig, eig, print_eigvals = True, print_eigvecs = False):
     E, Vec = np.linalg.eig(matrix_orig)
@@ -14,36 +11,6 @@ def numpy_check( matrix_orig, eig, print_eigvals = True, print_eigvecs = False):
         print("numpy results = ", E[:eig])
     if print_eigvecs :
         print("numpy results = ", Vec[:eig])
-
-def test_davidson():
-    threshold = 1e-8
-    max_iter = 500
-    ndim = 1000
-    nevals = 4
-    sparsity = 0.01
-    A = mu.make_diagonally_dominant(mu.generate_random_symmetric_matrix(ndim), sparsity)
-    B = A
-    dave_test = davidson.Davidson(A, "Davidson", nevals, threshold, max_iter)
-    dave_test.solve()
-    numpy_check(B, nevals)
-
-def test_jacobi_davidson():
-    threshold = 1e-8
-    max_iter = 50
-    ndim = 100
-    nevals = 4
-    sparsity = 0.001
-    A = mu.make_diagonally_dominant( mu.generate_random_symmetric_matrix(ndim), sparsity)
-    # Calculate and print eigvals from numpy for checking
-    numpy_check(A, nevals)
-    jd_test = jdr.JacobiDavidsonReal("Jacobi Davidson", nevals, threshold, max_iter)
-    jd_test.set_variables(preconditioning_type="Full")
-    jd_test.read_full_matrix(file_seedname="/home/RS_FILES/4C/full_mat")
-    jd_test.first_iteration_init()
-    jd_test.solve()
-
-    # Calculate and print eigvals from numpy for checking
-    numpy_check(A,nevals)
 
 def test_fortran_file_read():
     nrows, ncols = mat_reader.read_mat_info_file('/home/peter/SMALL_PROGS/FORTRAN_MAT_OUTPUT/mat1_test.info')
