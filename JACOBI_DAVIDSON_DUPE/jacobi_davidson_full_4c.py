42cc22d9c9a1b31ced107f1675bae17bdf4f1488
peterjohncherry <peter.cherry@northwestern.edu>
2019-08-01 18:39:01 +0200

    Building guess; why do you have so many nested functions???
    Redo this at some point, but keep for now to maintain consistency with TDA solver

--- JACOBI_DAVIDSON_DUPE/jacobi_davidson_full_4c.py
+++ JACOBI_DAVIDSON_DUPE/jacobi_davidson_full_4c.py
@@ -15,8 +15,53 @@ class JacobiDavidsonFull4C(eps_solvers.Solver):
             self.nvirt = int(self.ndims / 2) - self.nocc
 
         self.nov = self.nvirt * self.nocc
-        self.ndim = 2*(self.nocc*self.nvirt)
+        self.ndim = 2*self.nov
         print("self.ndim = ", self.ndim )
+
+        self.eindex = np.empty(self.nov)
+        for ii in range(self.nov):
+            self.eindex[ii] = ii
+
+        self.read_1e_eigvals_and_eigvecs(seedname = "/home/peter/RS_FILES/4C/1el_eigvals")
+
+        self.u_vecs = np.zeros((self.ndim, self.nev), dtype=np.complex64)
+        self.construct_guess()
+
+
+    def construct_guess(self):
+        for iev in range(self.nev):
+            self.u_vecs[:, iev] = self.tddft4_driver_guess(iev)
+
+    def read_1e_eigvals_and_eigvecs(self, seedname):
+        evals_1e_all = mr.read_fortran_array(seedname)
+        np.savetxt("/home/peter/MAT_TOOLS/JACOBI_DAVIDSON_DUPE/evals_orig.txt", evals_1e_all)
+
+        num_pos_evals = self.nvirt + self.nocc
+        print("num_pos_evals = ", num_pos_evals)
+        if self.pe_rot:
+            self.evals_1e = np.zeros(2*num_pos_evals, dtype=np.float64)
+            self.evals_1e[:num_pos_evals] = evals_1e_all[num_pos_evals:]
+            self.evals_1e[num_pos_evals:] = evals_1e_all[:num_pos_evals]
+        else:
+            self.evals_1e = np.zeros(num_pos_evals, dtype=np.float64)
+            self.evals_1e = evals_1e_all[num_pos_evals:]
+
+        np.savetxt("/home/peter/MAT_TOOLS/JACOBI_DAVIDSON_DUPE/evals_post.txt", self.evals_1e)
+        self.eindex = np.argsort(self.evals_1e)
+
+    # iguess : index of the eigenvector being built
+    def tddft4_driver_guess(self, iguess):
+        if self.P0_tsymm == "general":
+            return self.build_general_guess_vec(iguess)
+        elif self.P0_tsymm == "symmetric":
+            print( "Not implemented symmetric guess")
+            #return self.build_symmetric_guess_vec(iguess)
+
+    # guess used for open-shell systems
+    def build_general_guess_vec(self, iguess):
+        guess = np.zeros(self.ndim, dtype=np.complex64)
+        guess[self.eindex[iguess]] = 1.0+0.0j
+        return guess
 
     def solve(self):
         self.initialize_first_iteration()
@@ -40,8 +85,8 @@ class JacobiDavidsonFull4C(eps_solvers.Solver):
         self.u_hats = np.zeros_like(self.r_vecs)
 
         # Subspace Hamiltonian
-        self.submat= np.empty((2*self.maxs, 2*self.maxs), dtype=np.complex64)
+        self.submat = np.empty((2*self.maxs, 2*self.maxs), dtype=np.complex64)
         self.teta = np.zeros(self.nev, dtype=np.complex64)
 
     def main_loop(self):
-        print("nothing doing....")
+        print("main loop NOT IMPLEMENTED")
